671a37cd0e91b9391f762bd620a4d1276acf4643
Kashif <ahmed.kashif1001@gmail.com>
2024-01-01 12:41:33 +0500

    useReducer Hook

---
+++ 4-React-Hooks/usereducer-hook/src/App.jsx
@@ -0,0 +1,28 @@
+
+import './App.css'
+
+function App() {
+
+
+
+  const handleClickInc = () => {
+    set
+  }
+  
+  const handleClickDec = () => {
+    setCount
+  }
+ 
+
+  return (
+    <>
+     <h1>{count}</h1>
+
+    <button onClick={handleClickInc}></button> 
+    <button onClick={handleClickDec}></button>
+
+    </>
+  )
+}
+
+export default App
