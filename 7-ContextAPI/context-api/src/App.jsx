0773932c4c92a5479805dac8af8832ccec070f8c
Kashif <ahmed.kashif1001@gmail.com>
2024-01-02 22:39:08 +0500

    contextProvider and useContext

--- 7-ContextAPI/context-api/src/App.jsx
+++ 7-ContextAPI/context-api/src/App.jsx
@@ -1,10 +1,11 @@
-
+import { useContext } from 'react'
 import Counter from './components/Counter'
+import { CounterContext } from './context/Counter'
 
 function App() {
 
- 
-  
+ const counterContext = useContext(CounterContext)
+
   return (
     <>
     <div className="App">
