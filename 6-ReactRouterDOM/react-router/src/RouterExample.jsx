b96e23b57143ad4e5be2d5e4176dbb89a60f939c
Kashif <ahmed.kashif1001@gmail.com>
2024-01-03 22:03:22 +0500

    providing link to pages in navbar

--- 6-ReactRouterDOM/react-router/src/RouterExample.jsx
+++ 6-ReactRouterDOM/react-router/src/RouterExample.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Navbar from './components/Navbar'
+
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 
 const RouterExample = () => {
 
@@ -7,10 +13,10 @@ const RouterExample = () => {
   return (
     <>
       <BrowserRouter>
+      
+      <Navbar/>
         <Routes>
-          <Route path="/" element={<div>Home</div>}></Route>
-          <Route path="/contact" element={<div>Contact Us</div>}></Route>
-          <Route path="/about" element={<div>About Us</div>}></Route>
+
         </Routes>
       </BrowserRouter>
     </>
---
+++ 6-ReactRouterDOM/react-router/src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+const Navbar = () => {
+  return (
+    <div>
+        <ul>
+        <li>
+            <Link to="/">Home</Link>
+            </li>
+        <li>
+            <Link to="/about">About Us </Link>
+            </li>
+        <li>
+           <Link to="/contact"> Contact Us </Link>
+            </li>
+
+
+        </ul>
+
+
+
+
+
+
+    </div>
+  )
+}
+
+export default Navbar
