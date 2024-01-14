e7377af9a68410a87c2a91168dda5823d930ed8c
Kashif <ahmed.kashif1001@gmail.com>
2024-01-14 23:16:59 +0500

    components and jsx with project

---
+++ 1-Fundamentals/components-jsx/src/Book.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Book = () => {
+  return (
+ <>
+ 
+<img src='https://images-na.ssl-images-amazon.com/images/I/41AueBcq1CL._AC_SX184_.jpg'/>
+<h2>The Power of Subconcious Mind</h2>
+<p>By Amazing Ahmed</p>
+ 
+ </>
+  )
+}
+
+export default Book
---
+++ 1-Fundamentals/components-jsx/src/BookList.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Book from './Book'
+
+const BookList = () => {
+  return (
+    <>
+    
+   <Book/>
+    
+    </>
+  )
+}
+
+export default BookList
