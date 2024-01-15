7416f3a19595d9154241b3d6039217d1390b7a40
Kashif <ahmed.kashif1001@gmail.com>
2024-01-16 01:22:47 +0500

    props project complete

--- 1-Fundamentals/components-jsx/src/components/Book.jsx
+++ 1-Fundamentals/components-jsx/src/components/Book.jsx
@@ -1,22 +1,17 @@
 import React from "react";
 import "./Book.css";
 
-const Book = (props) => {
-  const image = "https://images-na.ssl-images-amazon.com/images/I/41AueBcq1CL._AC_SX184_.jpg";
-
-    const title = "Masters of the Air";
-    const publisher = "Amazing Ahmed"
-    const price = "$100"
+const Book = ({image, title, publisher, price}) => {
+  
 
   return (
     <div className="book">
-      {console.log(props)};
       <div className="book-image">
         <img src={image} />
       </div>
       <div className="book-info">
         <h2>{title}</h2>
-        <p className="book-author">by{publisher}</p>
+        <p className="book-author">by {publisher}</p>
         <p className="book-price">{price}</p>
 
         <button>Add to cart</button>
--- 1-Fundamentals/components-jsx/src/components/BookList.jsx
+++ 1-Fundamentals/components-jsx/src/components/BookList.jsx
@@ -1,20 +1,55 @@
-import React from 'react'
-import Book from './Book'
+import React from "react";
+import Book from "./Book";
+
+const bookOne = {
+  image:
+    "https://images-na.ssl-images-amazon.com/images/I/41AueBcq1CL._AC_SX184_.jpg",
+  title: "Masters of the Air",
+  publisher: "Shawn Ashley",
+  price: "$100",
+};
+
+const bookTwo = {
+  image:
+    "https://images-na.ssl-images-amazon.com/images/I/41AueBcq1CL._AC_SX184_.jpg",
+  title: "Rich Dad Poor Dad",
+  publisher: "Mitchell Darry",
+  price: "$300",
+};
+
+const bookThree = {
+  image:
+    "https://images-na.ssl-images-amazon.com/images/I/41AueBcq1CL._AC_SX184_.jpg",
+  title: "You can Negotiatie Anything",
+  publisher: "James Robert",
+  price: "$200",
+};
 
 const BookList = () => {
   return (
     <>
-    
-   <Book/>
-   <br/>
-   <br/>
-   <Book/>
-   <br/>
-   <br/>
-   <Book/>
-    
-    </>
-  )
-}
+      <Book
+        image={bookOne.image}
+        title={bookOne.title}
+        publisher={bookOne.publisher}
+        price={bookOne.price}
+      />
 
-export default BookList
+      <Book
+        image={bookTwo.image}
+        title={bookTwo.title}
+        publisher={bookTwo.publisher}
+        price={bookTwo.price}
+      />
+
+      <Book
+        image={bookThree.image}
+        title={bookThree.title}
+        publisher={bookThree.publisher}
+        price={bookThree.price}
+      />
+    </>
+  );
+};
+
+export default BookList;
