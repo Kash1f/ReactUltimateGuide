e3ed8f0d408c93ddfe55287a7415fec3c13a8787
Kashif <ahmed.kashif1001@gmail.com>
2024-01-18 23:41:58 +0500

    login registration

--- 1-Fundamentals/components-jsx/src/components/BookList.jsx
+++ 1-Fundamentals/components-jsx/src/components/BookList.jsx
@@ -1,42 +1,19 @@
 import React from "react";
 import Book from "./Book";
+import books from './../books.json'
 
-const books = [
-  {
-    image:
-      "https://images-na.ssl-images-amazon.com/images/I/41AueBcq1CL._AC_SX184_.jpg",
-    title: "Masters of the Air",
-    publisher: "Shawn Ashley",
-    price: "$100",
-  },
-
-  {
-    image:
-      "https://images-na.ssl-images-amazon.com/images/I/41AueBcq1CL._AC_SX184_.jpg",
-    title: "Rich Dad Poor Dad",
-    publisher: "Mitchell Darry",
-    price: "$300",
-  },
-
-  {
-    image:
-      "https://images-na.ssl-images-amazon.com/images/I/41AueBcq1CL._AC_SX184_.jpg",
-    title: "You can Negotiatie Anything",
-    publisher: "James Robert",
-    price: "$200",
-  },
-];
 const BookList = () => {
   return (
     <>
     {books.map((element)=>{
       return <Book
+      key={element.id}
       image={element.image}
       title={element.title}
       publisher={element.publisher}
       price={element.price}
+      id={element.id}
       />
-
 
     })}
       
