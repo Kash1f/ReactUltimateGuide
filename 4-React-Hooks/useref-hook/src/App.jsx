88843809dee1661222a4fc91fad4a33b618bfafb
Kashif <ahmed.kashif1001@gmail.com>
2024-01-19 23:27:42 +0500

    useref hook

---
+++ 4-React-Hooks/useref-hook/src/App.jsx
@@ -0,0 +1,16 @@
+import { useRef } from 'react'
+import './App.css'
+
+function App() {
+
+  const reference = useRef(initialValue);
+  console.log(reference);
+  return (
+    <>
+
+      Hello 
+    </>
+  )
+}
+
+export default App
