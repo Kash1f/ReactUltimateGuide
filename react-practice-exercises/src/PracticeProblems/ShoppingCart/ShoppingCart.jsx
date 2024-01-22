10ec7eeb19ab92ff0090d808a42f6a45d94210e2
Kashif <ahmed.kashif1001@gmail.com>
2024-01-22 22:17:49 +0500

    shopping cart project

---
+++ react-practice-exercises/src/PracticeProblems/ShoppingCart/ShoppingCart.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const ShoppingCart = () => {
+
+    const products = [
+        {id:1 , name: 'Product A'}
+        {id:1 , name: 'Product A'}
+        {id:1 , name: 'Product A'}
+        
+
+    ]
+
+
+  return (
+    <div>ShoppingCart</div>
+  )
+}
+
+export default ShoppingCart
