43da40bb9dd7e31828c28d3719825d7d99235199
Kashif <ahmed.kashif1001@gmail.com>
2024-01-28 17:26:19 +0500

    randomquote

--- react-practice-exercises/src/PracticeProblems/StopWatch.jsx
+++ react-practice-exercises/src/PracticeProblems/StopWatch.jsx
@@ -3,13 +3,16 @@ import { useState, useEffect, useRef} from 'react'
 
 const StopWatch = () => {
 
-    
 
+    const [isRunning, setIsRunning] = useState(false);
+    const [elapsedTime, setElapsedTime] = useState(0);
+    const intervalIdRef = useRef(null);
 
 
 
   return (
-    <div>StopWatch</div>
+    <>
+    </>
   )
 }
 
