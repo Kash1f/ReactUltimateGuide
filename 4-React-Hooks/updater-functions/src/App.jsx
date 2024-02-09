0712f3243bcd4fa5597fe4ec7e5f70e6ac34ab14
Kashif <ahmed.kashif1001@gmail.com>
2024-02-09 23:58:16 +0500

    updaterFunction

---
+++ 4-React-Hooks/updater-functions/src/App.jsx
@@ -0,0 +1,26 @@
+//updater function = A function passed as an argument to setState() usually ex. setYear(year+1)
+//                   Allow for safe updates based on the previous state
+//                   Used with multiple state updates and asynchronous functions
+//                   Good practice to use updater functions
+
+
+
+
+
+
+
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+   
+    </>
+  )
+}
+
+export default App
--- react-practice-exercises/src/PracticeProblems/FetchData.jsx
+++ react-practice-exercises/src/PracticeProblems/FetchData.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 const FetchData = () => {
   return (
     <div>FetchData from multiple APIs and display the combined data in your component</div>
+
+    
   )
 }
 
