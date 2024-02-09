ec8a2e78dcc9aee1c0d42d101e5b980900486f53
Kashif <ahmed.kashif1001@gmail.com>
2024-02-09 23:30:07 +0500

    mutltiple API fetch

---
+++ react-practice-exercises/src/PracticeProblems/FetchData.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FetchData = () => {
+  return (
+    <div>FetchData from multiple APIs and display the combined data in your component</div>
+  )
+}
+
+export default FetchData
