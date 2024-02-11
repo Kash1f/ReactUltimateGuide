41a7413a69641793f98efeaed8cc2cc73635505d
Kashif <ahmed.kashif1001@gmail.com>
2024-02-11 23:53:21 +0500

    update objects in state

--- 4-React-Hooks/updater-functions/src/App.jsx
+++ 4-React-Hooks/updater-functions/src/App.jsx
@@ -1,13 +1,7 @@
 //updater function = A function passed as an argument to setState() usually ex. setYear(year+1)
 //                   Allow for safe updates based on the previous state
 //                   Used with multiple state updates and asynchronous functions
-//                   Good practice to use updater functions
-
-
-
-
-
-
+//                   Good practice to use updater function
 
 import { useState } from 'react'
 
