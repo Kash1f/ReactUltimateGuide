db5edc5f7b51ae778eb47c9d3d833f7defe16396
Kashif <ahmed.kashif1001@gmail.com>
2024-02-24 00:00:13 +0500

    updating title using reactrouter

---
+++ react-practice-exercises/src/PracticeProblems/Title.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {BrowserRouter} from 'react-router-dom'
+
+const Title = () => {
+  return (
+    <BrowserRouter>
+    </BrowserRouter>
+  )
+}
+
+export default Title
