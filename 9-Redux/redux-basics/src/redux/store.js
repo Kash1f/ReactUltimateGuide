23923c861b11a6a425b06b99f3702430b15c7878
Kashif <ahmed.kashif1001@gmail.com>
2024-04-09 01:44:22 +0500

    reducer functions

--- 9-Redux/redux-basics/src/redux/store.js
+++ 9-Redux/redux-basics/src/redux/store.js
@@ -1,5 +1,7 @@
 import {legacy_createStore} from 'redux'
 
+// state: jo hamari abhi state chal rahi hai means 0 hai ya 1
+// action: user ne jo increment kiya hai ya decrement
 const reducer = (state = 0, action) => {
     switch(action.type){
         case 'INCREMENT' : return state + 1;
@@ -7,6 +9,10 @@ const reducer = (state = 0, action) => {
         default: return state;
 }
 }
+
+//1. Is store ko puray app ka access dena hai
+
+//2. index.js/main.jsx mai -Provider- me wrap kardengy is store ko. App Provider k andar wrap ho chuka hai and puray App k paas store ka access hai.
 
 const store = legacy_createStore(reducer);
 
