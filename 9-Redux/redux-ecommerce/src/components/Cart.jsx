beb2b0bf49cba68d5c08730d746e155cfafec850
Kashif <ahmed.kashif1001@gmail.com>
2024-04-13 02:36:58 +0500

    added prdoucts json, cart and products ui

--- 9-Redux/redux-ecommerce/src/components/Cart.jsx
+++ 9-Redux/redux-ecommerce/src/components/Cart.jsx
@@ -1,9 +1,13 @@
 
 
-const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
 
-export default Cart
+const Cart = () => {
+ 
+  return (
+    <div className="alert alert-success">
+      <h3 className="text-center">Total Items: 5 (Rs. 3,000)/-</h3>
+    </div>
+  );
+};
+
+export default Cart;
