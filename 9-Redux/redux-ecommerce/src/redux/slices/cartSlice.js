9ccc93b10d58d0ce3ee349e0bd68aeb2c2db6b0f
Kashif <ahmed.kashif1001@gmail.com>
2024-04-15 23:10:16 +0500

    payloads, cartReducer

---
+++ 9-Redux/redux-ecommerce/src/redux/slices/cartSlice.js
@@ -0,0 +1,32 @@
+//what is a slice?
+
+// if an app has a lot of features like cart feature and chat feature so hum har feature ka apna slice bana sakte hain
+
+//is cartSlice k andar jitne slices banengy wo cart se related banengy
+
+import { createSlice } from '@reduxjs/toolkit'
+
+const cartSlice = createSlice({
+    name: "Cart",
+    initialState:[],    //empty array because is me items hongy
+    reducers: {
+        addItem: (state,action)=>{
+            state = [...state, action.payload];   //existing state spread and last me new items ko daaldia via action.payload
+        }
+    }
+})
+
+//Reducers:
+
+//what can be the first reducer of the cart?
+//Ans: To add items to the cart
+
+//reducer will have addtoCart function with 2 parameters, state and action.
+
+//what is state in this addItem function?
+//hamari state aik empty array hai by default
+
+//what is action in this addItem function?
+//jo bhi user payload waghera send karega
+
+export default cartSlice.reducer;
