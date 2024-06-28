6aab3ed9cece04128bd9849ad8f25cfc7ec9723a
Kashif <ahmed.kashif1001@gmail.com>
2024-06-28 23:59:16 +0500

    state and events

--- React Native Guide/1-Basics/task-app/App.js
+++ React Native Guide/1-Basics/task-app/App.js
@@ -1,20 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
 export default function App() {
+
+   const [task, setTask] = useState('')
+   const [taskList, setTaskList] = ([])
+
+   const inputHandler = (enteredText) => {
+      setTask(enteredText)
+   }
+
+   const addTaskButton = () => {
+    setTaskList()
+   }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput onChangeText={inputHandler} style={styles.inputBox} placeholder='Add Your Tasks'/>
+        <Button title="Add Task"></Button>
+      </View>
+      <View>
+        <Text style={styles.textHeading}>Your Tasks: </Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 15
   },
+  inputContainer : {
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+  inputBox : {
+    width: '75%',
+    borderWidth: 1,
+    borderColor:'#cccccc',
+    borderRadius: 15,
+    padding: 5,
+    paddingLeft: 12
+  },
+  textHeading : {
+    marginTop:5, 
+    fontSize: 15, 
+    fontFamily:'sans-serif',
+    fontWeight: 'bold',
+    marginLeft:8
+  }
+
 });
