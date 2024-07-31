65b6e72a8b5eb4191a9a98423cd6c305dca3bc7f
Kashif <ahmed.kashif1001@gmail.com>
2024-07-31 23:56:29 +0500

    react native interview questions

--- React Native Guide/Interview Questions/basic-questions/App.js
+++ React Native Guide/Interview Questions/basic-questions/App.js
@@ -1,23 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-
-  const getApiData = async () => {
-    //api call
-    const url = "https://jsonplaceholder.typicode.com/posts/1";
-    let result = await fetch(url);
-    result = await result.json();
-    console.warn(result)
-  }
-  useEffect(()=>{
-    getApiData();
-  },[])
-
   return (
     <View style={styles.container}>
-      <Text>Fetch Api!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
