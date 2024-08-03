636ae8949c811338fc9664d9429e0faf5493a354
Kashif <ahmed.kashif1001@gmail.com>
2024-08-03 18:18:18 +0500

    core and custom components

---
+++ React Native Guide/Interview Questions/core-custom-components/ComponentA.js
@@ -0,0 +1,25 @@
+import { ScrollView, Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+
+//Core Components:
+
+// Text
+// View
+// ScrollView
+// Image
+// TextInput
+
+
+
+export class ComponentA extends Component {
+  render() {
+    return (
+      <View>
+        <Text>ComponentA</Text>
+      </View>
+    )
+  }
+}
+
+export default ComponentA
