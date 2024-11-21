7de7c22d12816ac90f8700a9db80b7ea49fe3215
Kashif <ahmed.kashif1001@gmail.com>
2024-11-21 13:40:20 +0500

    home cards

--- React Complete Guide/14-Project/job-website/src/App.jsx
+++ React Complete Guide/14-Project/job-website/src/App.jsx
@@ -1,47 +1,20 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import HomeCards from "./components/HomeCards";
 
 const App = () => {
   return (
     <>
       <Navbar/>
-      <Hero/>
-
+      <Hero title="Become a React Dev" subtitle="Find the React job that fits your skills and needs" />
+      <HomeCards/>
+      
       
 
 
-      {/* <!-- Developers and Employers --> */}
-      <section className="py-4">
-        <div className="container-xl lg:container m-auto">
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
-            <div className="bg-gray-100 p-6 rounded-lg shadow-md">
-              <h2 className="text-2xl font-bold">For Developers</h2>
-              <p className="mt-2 mb-4">
-                Browse our React jobs and start your career today
-              </p>
-              <a
-                href="/jobs.html"
-                className="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
-              >
-                Browse Jobs
-              </a>
-            </div>
-            <div className="bg-indigo-100 p-6 rounded-lg shadow-md">
-              <h2 className="text-2xl font-bold">For Employers</h2>
-              <p className="mt-2 mb-4">
-                List your job to find the perfect developer for the role
-              </p>
-              <a
-                href="/add-job.html"
-                className="inline-block bg-indigo-500 text-white rounded-lg px-4 py-2 hover:bg-indigo-600"
-              >
-                Add Job
-              </a>
-            </div>
-          </div>
-        </div>
-      </section>
+     
+      
 
       {/* <!-- Browse Jobs --> */}
       <section className="bg-blue-50 px-4 py-10">
@@ -177,3 +150,13 @@ export default App;
     {loggedIn ? <p>Logged In</p> : <p>Not Logged In</p>}
     */
 }
+
+
+
+
+// 1. Navbar
+// 2. Hero
+//3. keep the app file clean and make a separate cardwrapper component and have both the cards in there
+// <HomeCards>
+//   <Card1>
+//     <Card2>
