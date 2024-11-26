38b8d4148b4054fd07b9b1928589b8862edc960c
Kash1f <ahmed.kashif1001@gmail.com>
2024-11-26 17:19:08 +0500

    async await axios get

--- React Complete Guide/10-Axios/axios-project/src/App.jsx
+++ React Complete Guide/10-Axios/axios-project/src/App.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import { useEffect } from "react";
+import Movies from "./pages/Movies";
+import axios from "axios";
 
 const App = () => {
-  return (
-  <>
-  <Movie/>
-  </>
-  )
-}
+  const API = "https://omdapi.com/?i=tt";
 
-export default App
+  const getMovies = async () => {
+    //always use try catch to handle errors
+    try {
+
+      //the response is stored in the variable when the promise is resolved, the await keyword waits for the promise to resolve, then the code inside the try block is executed, if the promise is rejected, the catch block is executed, and the error is logged to the console. 
+      const response = await axios.get(API);
+      console.log(response.data);
+
+    } catch (error){
+      console.log(error);
+    }
+
+  }
+  //when the page loads, the useEffect runs once and the getMovies function is called, [] array to make sure that useEffect only runs once
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return (
+    <>
+      <Movies />
+    </>
+  );
+};
+
+export default App;
--- React Complete Guide/10-Axios/axios-project/src/pages/Movies.jsx
+++ React Complete Guide/10-Axios/axios-project/src/pages/Movies.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Movies = () => {
   return (
-    <div>
+    <h1>
       Movie List
-    </div>
+    </h1>
   )
 }
 
