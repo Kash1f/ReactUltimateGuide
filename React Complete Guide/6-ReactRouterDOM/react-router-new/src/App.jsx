db24f7554f5457b08ee1f396bc95914f0826a144
Kash1f <ahmed.kashif1001@gmail.com>
2024-11-29 20:45:52 +0500

    updated code

--- React Complete Guide/6-ReactRouterDOM/react-router-new/src/App.jsx
+++ React Complete Guide/6-ReactRouterDOM/react-router-new/src/App.jsx
@@ -1,38 +1,49 @@
 import React from "react";
 
-import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Dashboard from "./components/Dashboard";
 import Navbar from "./components/Navbar";
 
-// Layout Component
-const Layout = () => (
-  <>
-    <Navbar /> {/* Navbar is rendered once here */}
-    <Outlet /> {/* Outlet renders the matched child route */}
-  </>
-);
-
 const router = createBrowserRouter([
   {
-    path: "/", // Layout route
-    element: <Layout />, // Layout wraps all child routes
-    children: [
-      { path: "/", element: <Home /> },
-      { path: "/about", element: <About /> },
-      { path: "/dashboard", element: <Dashboard /> },
-    ],
+    path: "/", // The path of the route
+    // The component to be rendered on this path
+    element: (
+      <>
+        <Navbar />
+        <Home />
+      </>
+    ),
+  },
+  {
+    path: "/about", // The path of the route
+    element: (
+      <>
+        <Navbar />
+        <About />
+      </>
+    ),
+  },
+  {
+    path: "/dashboard", // The path of the route
+    element: (
+      <>
+        <Navbar />
+        <Dashboard />
+      </>
+    ),
   },
 ]);
 
 const App = () => {
   return (
-    <RouterProvider router={router} />
+    <div>
+     
+      <RouterProvider router={router} />
+    </div>
   );
 };
 
 export default App;
-
-
-
