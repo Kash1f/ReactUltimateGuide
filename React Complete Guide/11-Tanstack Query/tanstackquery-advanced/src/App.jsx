8451699c8b94b780c23d0dce63432008c5d465b0
Kash1f <ahmed.kashif1001@gmail.com>
2024-12-13 16:00:13 +0500

    layout update

--- React Complete Guide/11-Tanstack Query/tanstackquery-advanced/src/App.jsx
+++ React Complete Guide/11-Tanstack Query/tanstackquery-advanced/src/App.jsx
@@ -1,32 +1,33 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
+import { AppLayout } from "./components/Layout/AppLayout";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-
-
-    const router = createBrowserRouter([
+const router = createBrowserRouter([
       {
         path: "/",
-        element: <MainLayout />,
+        element: <AppLayout />,
         children: [
           {
             path: "/",
             element: <Home />,
           },
-          {
-            path: "/trad",
-            element: <FetchOld />,
-          },
-          {
-            path: "/rq",
-            element: <FetchRQ />,
-          },
-          {
-            path: "/rq/:id",
-            element: <FetchIndv />,
-          },
-          {
-            path: "/infinite",
-            element: <InfiniteScroll />,
-          },
+          // {
+          //   path: "/trad",
+          //   element: <FetchOld />,
+          // },
+          // {
+          //   path: "/rq",
+          //   element: <FetchRQ />,
+          // },
+          // {
+          //   path: "/rq/:id",
+          //   element: <FetchIndv />,
+          // },
+          // {
+          //   path: "/infinite",
+          //   element: <InfiniteScroll />,
+          // },
         ],
       },
     ]);
@@ -36,7 +37,6 @@ import Home from "./pages/Home";
       return (
         <QueryClientProvider client={queryClient}>
           <RouterProvider router={router}></RouterProvider>
-          <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
       );
     };
--- React Complete Guide/11-Tanstack Query/tanstackquery-advanced/src/components/Layout/AppLayout.jsx
+++ React Complete Guide/11-Tanstack Query/tanstackquery-advanced/src/components/Layout/AppLayout.jsx
@@ -1,9 +1,9 @@
 
 import { Outlet } from "react-router-dom";
-import { Header } from "./Header";
-import { Footer } from "./Footer";
+import  Header from "./Header";
+import  Footer  from "./Footer";
 
-export const MainLayout = () => {
+export const AppLayout = () => {
   return (
     <>
       <Header />
