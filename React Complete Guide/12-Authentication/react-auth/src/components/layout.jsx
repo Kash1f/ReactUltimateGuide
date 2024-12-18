d15c7dbcd738234324cad1c3a24eab1de5411738
Kashif <ahmed.kashif1001@gmail.com>
2024-12-19 00:01:32 +0500

    header, layout

--- React Complete Guide/12-Authentication/react-auth/src/components/layout.jsx
+++ React Complete Guide/12-Authentication/react-auth/src/components/layout.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Header from './header'
+import { Outlet } from 'react-router-dom'
 
 const layout = () => {
   return (
-    <div>layout</div>
+    <>
+    <Header/>
+    <Outlet/>
+    </>
   )
 }
 
