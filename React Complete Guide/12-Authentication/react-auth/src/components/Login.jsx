90d38c80a69224b71f8601934375c52d41758327
Kash1f <ahmed.kashif1001@gmail.com>
2024-12-20 19:09:18 +0500

    login page for authentication

--- React Complete Guide/12-Authentication/react-auth/src/components/Login.jsx
+++ React Complete Guide/12-Authentication/react-auth/src/components/Login.jsx
@@ -3,7 +3,27 @@ import React from "react";
 const Login = () => {
   return (
     <>
-     <div className="font-bold text-2xl bg-red-700">Login</div>
+      <div className="bg-sky-700 border shadow-md space-y-4 p-5 rounded-md m-20 w-[250px]">
+      <p>Login Page</p>
+
+      <div>
+        <p>Email</p>
+        <input type="email" className="border rounded-md shadow-md"/>
+      </div>
+
+      <div>
+        <p>Password</p>
+        <input type="password" className="border rounded-md shadow-md"/>
+      </div>
+      
+      <div className="flex justify-center">
+      <button className="bg-blue-500 text-white rounded-md p-2 shadow-md">Login</button>
+      </div>
+
+
+
+
+      </div>
     </>
   );
 };
