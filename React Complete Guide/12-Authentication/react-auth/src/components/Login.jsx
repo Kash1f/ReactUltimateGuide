0fb6b23292e08e2937e24716afccfb5a4abe96cf
Kash1f <ahmed.kashif1001@gmail.com>
2024-12-20 19:29:23 +0500

    payload object email password

--- React Complete Guide/12-Authentication/react-auth/src/components/Login.jsx
+++ React Complete Guide/12-Authentication/react-auth/src/components/Login.jsx
@@ -1,23 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const payload = {
+      email: email,
+      password: password,
+    };
+    console.log(payload);
+    
+  };
+
   return (
     <>
       <div className="bg-sky-700 border shadow-md space-y-4 p-5 rounded-md m-20 w-[250px]">
-      <p>Login Page</p>
+      <p className="font-semibold text-center">Login Page</p>
 
       <div>
         <p>Email</p>
-        <input type="email" className="border rounded-md shadow-md"/>
+        <input onChange={(e)=>setEmail(e.target.value)}  type="email" className="border rounded-md shadow-md"/>
       </div>
 
       <div>
         <p>Password</p>
-        <input type="password" className="border rounded-md shadow-md"/>
+        <input onChange={(e)=>setPassword(e.target.value)} type="password" className="border rounded-md shadow-md"/>
       </div>
       
       <div className="flex justify-center">
-      <button className="bg-blue-500 text-white rounded-md p-2 shadow-md">Login</button>
+      <button onClick={handleSubmit} className="bg-blue-500 text-white rounded-md px-4 py-1 shadow-md">Login</button>
       </div>
 
 
@@ -29,3 +43,8 @@ const Login = () => {
 };
 
 export default Login;
+
+
+//jo bhi user type karega wo setEmail and setPassword me store hoga then email and password state variables me aur humay screen pe show hojaiga
+
+//jab button k click pe handleSubmit run hoga to email and password ki jo setEmail and setPassword se value aayi hogi wo value in dono current variables me ajaigi
