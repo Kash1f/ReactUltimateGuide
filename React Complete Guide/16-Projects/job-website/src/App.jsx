42e500de7159720ef8bbc54610ff3efacf68b1cd
Kashif <ahmed.kashif1001@gmail.com>
2024-12-31 12:04:47 +0500

    app routing, router provider

--- React Complete Guide/16-Projects/job-website/src/App.jsx
+++ React Complete Guide/16-Projects/job-website/src/App.jsx
@@ -4,19 +4,33 @@ import Hero from "./components/Hero";
 import HomeCards from "./components/HomeCards";
 import JobListings from "./components/JobListings";
 import ViewJobs from "./components/ViewJobs";
+import { Route } from "react-router-dom";
+
 
 const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <HomeCards />
-      <JobListings />
-      <ViewJobs/>
-    </>
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path='/jobs' element={<JobsPage />} />
+        <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />} />
+        <Route
+          path='/edit-job/:id'
+          element={<EditJobPage updateJobSubmit={updateJob} />}
+          loader={jobLoader}
+        />
+        <Route
+          path='/jobs/:id'
+          element={<JobPage deleteJob={deleteJob} />}
+          loader={jobLoader}
+        />
+        <Route path='*' element={<NotFoundPage />} />
+      </Route>
+    )
   );
-};
 
+  return <RouterProvider router={router} />;
+};
 export default App;
 
 
