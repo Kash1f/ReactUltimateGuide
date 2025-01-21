c8d956975cf9fbd09bac98331af9741e13437e4b
Kash1f <ahmed.kashif1001@gmail.com>
2025-01-21 18:02:17 +0500

    crud apis

--- React Complete Guide/11-Tanstack Query/react-queryv5/src/services/api.ts
+++ React Complete Guide/11-Tanstack Query/react-queryv5/src/services/api.ts
@@ -3,3 +3,33 @@ export const getTodos = async () => {
     const data = await response.json(); 
     return data;
 }
+
+export const getTodo = async (id: number) => {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
+    const data = await response.json(); 
+    return data;
+}
+
+export const createTodo = async (data: any) => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+    const data = await response.json(); 
+    return data;
+}
+
+export const updateTodo = async (id: number, data: any) => {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+    const data = await response.json(); 
+    return data;
+}
