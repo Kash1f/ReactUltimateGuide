bc745293233d0d82902a958579d44bfe4a52b81f
Kash1f <ahmed.kashif1001@gmail.com>
2025-01-22 21:03:01 +0500

    added crud apis

--- React Complete Guide/11-Tanstack Query/react-queryv5/src/services/api.ts
+++ React Complete Guide/11-Tanstack Query/react-queryv5/src/services/api.ts
@@ -1,11 +1,26 @@
 import axios from "axios";
 import { Todo } from "../types/todo";
 
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://jsonplaceholder.typicode.com/";
 const api = axios.create({ baseURL: BASE_URL });
 
 export const getTodosIds = async () => {
   return (await api.get<Todo[]>("todos")).data.map((todo) => todo.id);
 };
 
+export const getTodo = async (id: number) => {
+  return (await api.get<Todo>(`todos/${id}`)).data;
+};
+
+export const createTodo = async (data: Todo) => {
+  await api.post("todos", data);
+};
+
+export const updateTodo = async (data: Todo) => {
+  await api.put(`todos/${data.id}`, data);
+};
+
+export const deleteTodo = async (id: number) => {
+  await api.delete(`todos/${id}`);
+};
 
