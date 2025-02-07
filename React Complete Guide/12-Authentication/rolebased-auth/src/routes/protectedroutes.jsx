90ceb48214a71e6dfdb5f1fd77fe14b74112d71b
Kashif <ahmed.kashif1001@gmail.com>
2025-02-07 23:42:33 +0500

    protected routes user role based implementation

--- React Complete Guide/12-Authentication/rolebased-auth/src/routes/protectedroutes.jsx
+++ React Complete Guide/12-Authentication/rolebased-auth/src/routes/protectedroutes.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from '../context/ContextProvider'
+import { Navigate } from 'react-router-dom';
 
-const ProtectedRoutes = () => {
+const ProtectedRoutes = ({children, roles}) => {
+
+  const {role, authenticated} = useContext(UserContext);
+
+  if(!authenticated) {  
+    return <Navigate to="/login" />
+  }
+
+  if(!roles.includes(role)) {
+    return <Navigate to="/unauthorized" />
+  }
+
   return (
     <div>ProtectedRoutes</div>
   )
